ef5211a1b135fe5f6d1a1f7ac060c8d257b37287
erik schievenin <erik.schievenin@gmail.com>
2022-03-14 16:01:16 +0100

    first exercise complete

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 console.log('JS OK!');
+
+// ************Palidroma*************
+// Chiedere all’utente di inserire una parola
+
+// LOGICA PER SVOLGERE L'ESERCIZIO
+
+/*let word = prompt('Inserisci una parola palindroma').toLowerCase().split('');
+console.log(word);
+
+let wordReverse = word.reverse();
+console.log(wordReverse);
+
+let wordJoin = wordReverse.join('');
+console.log(wordJoin);
+
+if (word === wordReverse) {
+    console.log('la parola è palindroma');
+} else {
+    console.log('la parola non è palindroma');
+}*/
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+let word = prompt('Inserisci una parola palindroma').toLowerCase();
+
+let wordJoin = parolaPalindroma(word);
+
+if (word === wordJoin) {
+    console.log('la parola è palindroma');
+} else {
+    console.log('la parola non è palindroma');
+}
+
+function parolaPalindroma(ciccio) {
+    let ciccioInverso = ciccio.split('').reverse().join('');
+    return ciccioInverso;
+}
