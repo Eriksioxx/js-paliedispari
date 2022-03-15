efde700a88fc15227bd6eca99c5861e39442919c
erik schievenin <erik.schievenin@gmail.com>
2022-03-15 11:34:21 +0100

    Correction exercise

--- palindroma/script.js
+++ palindroma/script.js
@@ -10,7 +10,7 @@ console.log(word);
 
 let wordReverse = word.reverse();
 console.log(wordReverse);
-
+  
 let wordJoin = wordReverse.join('');
 console.log(wordJoin);
 
@@ -24,15 +24,41 @@ if (word === wordReverse) {
 
 let word = prompt('Inserisci una parola palindroma').toLowerCase();
 
-let wordJoin = parolaPalindroma(word);
+while (!word || word.length === 0) {
+    word = prompt('Inserisci una parola palindroma').toLowerCase();
+}
 
-if (word === wordJoin) {
+console.log(word);
+
+// Dobbiamo fare un reverse della parola inserita e 
+// confrontarla con quella dell'utente
+
+const reverseWord = invertWord(word);
+console.log(reverseWord);
+
+
+
+function invertWord(wordArgument) {
+
+    let inverted = '';
+
+    // creo un ciclo che va da 0 alla lunghezza in caratteri della stringa
+    for (let i = 0; i < wordArgument.length; i++) {
+        // recupero il carattre all'interno della stringa alla posizione i 
+        // (0 indica il primo carattere, 1 al secondo carattere e cosi via...)
+        const char = wordArgument.charAt(i);
+        console.log(char);
+
+        inverted = char + inverted;
+    }
+
+    return inverted;
+}
+
+// Controllo se è o no palindroma 
+
+if (word === reverseWord) {
     console.log('la parola è palindroma');
 } else {
     console.log('la parola non è palindroma');
-}
-
-function parolaPalindroma(ciccio) {
-    let ciccioInverso = ciccio.split('').reverse().join('');
-    return ciccioInverso;
 }
